d8d5cb848c9334171e448847c19020e8d5dfaf3f
ystuder <omega_3@gmx.ch>
2015-04-24 15:04:16 +0200

    Erster Commit mit den BLDC-Spezifischen Daten

---
+++ BLDC/BLDC/Sources/BLDC.h
@@ -0,0 +1,52 @@
+/*
+ * BLDC.h
+ *
+ *  Created on: Mar 7, 2015
+ *      Author: Ninux
+ */
+
+#ifndef BLDC_H_
+#define BLDC_H_
+
+#include "FRTOS1.h"
+
+#define BLDC_PARSE_COMMAND_ENABLED 1 /* set to 1 if method ParseCommand()
+					is present, 0 otherwise */
+
+#define CS_HANDELD_BY_CODEEXPERT 0
+
+#if CS_HANDELD_BY_CODEEXPERT == 0
+#define MORE_AS_ONE_SLAVE 1
+#endif
+typedef enum
+{
+	ON,
+	OFF
+}MotorState;
+
+typedef enum
+{
+	BLDC1,
+	BLDC2
+}BldcMotors_t;
+
+void BLDC_init(void);
+
+void setMotor(BldcMotors_t m);
+uint8_t putBLDC(MotorState s);
+uint8_t setSpeed(uint16_t val);
+
+
+void BLDC_Receive_from_spi(void);
+byte BLDC_ParseCommand(const unsigned char *cmd, bool *handled, const CLS1_StdIOType *io);
+
+int BLDC_get_enable(void);
+
+int BLDC_get_rpm(void);
+
+void bldc1_irq_occurred(void);
+#if MORE_AS_ONE_SLAVE
+void bldc2_irq_occurred(void);
+#endif
+
+#endif /* BLDC_H_ */
---
+++ BLDC/BLDC/Sources/Events.c
@@ -0,0 +1,306 @@
+/* ###################################################################
+**     Filename    : Events.c
+**     Project     : uC_Host
+**     Processor   : MKL25Z128VLK4
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2015-03-06, 09:12, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMIINT - void Cpu_OnNMIINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.c
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+/* MODULE Events */
+
+#include "Cpu.h"
+#include "Events.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+#include "BLDC.h"
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT (module Events)
+**
+**     Component   :  Cpu [MKL25Z128LK4]
+*/
+/*!
+**     @brief
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the [NMI
+**         interrupt] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnNMIINT(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  FRTOS1_vApplicationStackOverflowHook (module Events)
+**
+**     Component   :  FRTOS1 [FreeRTOS]
+**     Description :
+**         if enabled, this hook will be called in case of a stack
+**         overflow.
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**         pxTask          - Task handle
+**       * pcTaskName      - Pointer to task name
+**     Returns     : Nothing
+** ===================================================================
+*/
+void FRTOS1_vApplicationStackOverflowHook(xTaskHandle pxTask, char *pcTaskName)
+{
+  /* This will get called if a stack overflow is detected during the context
+     switch.  Set configCHECK_FOR_STACK_OVERFLOWS to 2 to also check for stack
+     problems within nested interrupts, but only do this for debug purposes as
+     it will increase the context switch time. */
+  (void)pxTask;
+  (void)pcTaskName;
+  taskDISABLE_INTERRUPTS();
+  /* Write your code here ... */
+  for(;;) {}
+}
+
+/*
+** ===================================================================
+**     Event       :  FRTOS1_vApplicationTickHook (module Events)
+**
+**     Component   :  FRTOS1 [FreeRTOS]
+**     Description :
+**         If enabled, this hook will be called by the RTOS for every
+**         tick increment.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void FRTOS1_vApplicationTickHook(void)
+{
+  /* Called for every RTOS tick. */
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  FRTOS1_vApplicationIdleHook (module Events)
+**
+**     Component   :  FRTOS1 [FreeRTOS]
+**     Description :
+**         If enabled, this hook will be called when the RTOS is idle.
+**         This might be a good place to go into low power mode.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void FRTOS1_vApplicationIdleHook(void)
+{
+  /* Called whenever the RTOS is idle (from the IDLE task).
+     Here would be a good place to put the CPU into low power mode. */
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  FRTOS1_vApplicationMallocFailedHook (module Events)
+**
+**     Component   :  FRTOS1 [FreeRTOS]
+**     Description :
+**         If enabled, the RTOS will call this hook in case memory
+**         allocation failed.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void FRTOS1_vApplicationMallocFailedHook(void)
+{
+  /* Called if a call to pvPortMalloc() fails because there is insufficient
+     free memory available in the FreeRTOS heap.  pvPortMalloc() is called
+     internally by FreeRTOS API functions that create tasks, queues, software
+     timers, and semaphores.  The size of the FreeRTOS heap is set by the
+     configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
+  taskDISABLE_INTERRUPTS();
+  /* Write your code here ... */
+  for(;;) {}
+}
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnSupervisorCall (module Events)
+**
+**     Component   :  Cpu [MKL25Z128LK4]
+*/
+/*!
+**     @brief
+**         This event is called when the Supervisor Call exception had
+**         occurred. This event is automatically enabled when the
+**         [Supervisor Call] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnSupervisorCall(void)
+{
+	vPortSVCHandler();
+}
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnPendableService (module Events)
+**
+**     Component   :  Cpu [MKL25Z128LK4]
+*/
+/*!
+**     @brief
+**         This event is called when the Pendable Service exception had
+**         occurred. This event is automatically enabled when the
+**         [Pendable Service] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnPendableService(void)
+{
+	vPortPendSVHandler();
+}
+
+/*
+** ===================================================================
+**     Event       :  CLS1_OnBeforeIterateCmd (module Events)
+**
+**     Component   :  CLS1 [uC_Host]
+**     Description :
+**         Hook called before parsing a command in IterateTable().
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**         cmd             - command parsed
+**     Returns     : Nothing
+** ===================================================================
+*/
+void CLS1_OnBeforeIterateCmd(const uint8_t *cmd)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  CLS1_OnAfterIterateCmd (module Events)
+**
+**     Component   :  CLS1 [uC_Host]
+**     Description :
+**         Hook called after parsing a command in IterateTable().
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**         cmd             - command parsed
+**     Returns     : Nothing
+** ===================================================================
+*/
+void CLS1_OnAfterIterateCmd(const uint8_t *cmd)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  BLDCspi_OnRxChar (module Events)
+**
+**     Component   :  BLDCspi [SynchroMaster]
+**     Description :
+**         This event is called after a correct character is received.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void BLDCspi_OnRxChar(void)
+{
+  /* Write your code here ... */
+	BLDC_Receive_from_spi();
+}
+
+/*
+** ===================================================================
+**     Event       :  BLDCspi_OnTxChar (module Events)
+**
+**     Component   :  BLDCspi [SynchroMaster]
+**     Description :
+**         This event is called after a character is transmitted.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void BLDCspi_OnTxChar(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  BLDC2_IRQ_OnInterrupt (module Events)
+**
+**     Component   :  BLDC2_IRQ [ExtInt]
+**     Description :
+**         This event is called when an active signal edge/level has
+**         occurred.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void BLDC2_IRQ_OnInterrupt(void)
+{
+#if MORE_AS_ONE_SLAVE
+	bldc2_irq_occurred();
+#endif
+}
+
+/*
+** ===================================================================
+**     Event       :  BLDC1_IRQ_OnInterrupt (module Events)
+**
+**     Component   :  BLDC1_IRQ [ExtInt]
+**     Description :
+**         This event is called when an active signal edge/level has
+**         occurred.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void BLDC1_IRQ_OnInterrupt(void)
+{
+	bldc1_irq_occurred();
+}
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.4 [05.10]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
